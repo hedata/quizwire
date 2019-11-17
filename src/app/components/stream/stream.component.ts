822e74e9ef036ca159e22646305add7e274b2b85
hedata <erichheil@gmail.com>
2019-11-17 01:30:25 +0100

    show subredit and better image versions

--- src/app/components/stream/stream.component.ts
+++ src/app/components/stream/stream.component.ts
@@ -46,31 +46,78 @@ export class StreamComponent implements OnInit {
 
   ngOnInit() {
     console.log('[Stream] Init');
-    const intervall = 2300;
+    //2300
+    const intervall = 5300;
       setInterval(this.loadData, intervall);
   }
 
   private loadData = async () => {
     const currentDate:any = new Date();
     if(this.isActive) {
-      console.log("[Stream] loding data");
-      const countoldItems = this.streamItems.length
-      this.streamItems = (await this.dataService.getLatestRawStream({
+      //console.log("[Stream] loding data");
+      const countoldItems = this.streamItems.length;
+      const item = (await this.dataService.getLatestRawStream({
         lastUpdate : this.streamItems.length>0?this.streamItems[0].created_at:null,
         limit : 1
-      })).concat(this.streamItems);
-
+      }))[0];
+      //add displayType and displayUrl
+      this.addTypes(item);
+      console.log(item.display,item.url,item);
+      this.streamItems.unshift(item);
+      if(this.streamItems.length>500) {
+        this.streamItems.pop();
+      }
       const diff = (currentDate - this.lastDate) / 1000;
-      console.log(this.streamItems.length, "after: ", diff);
-      console.log("Start: ",this.lastDate, " End: ",currentDate);
-      //console.log(this.streamItems.filter((el:any)=>el.imageUrl));
+      console.log("after: ", diff);
+      //console.log("Start: ",this.lastDate, " End: ",currentDate);
+
       this.lastDate = currentDate;
       this.fade=false;
       setTimeout(()=> {
-        console.log("[Fade]")
+        //console.log("[Fade]")
         this.fade = true;
       },1000)
     }
 
+  }
+  addTypes = (item:any) => {
+    item.display =  {
+      displayType : this.getDisplayType(item),
+      displayUrl : null
+    };
+    item.display.displayUrl = this.getDisplayUrl(item);
+    return item;
+  }
+  private getDisplayUrl = (item:any) => {
+    if(item.display.displayType==='image') {
+      if(item.url) {
+        if(item.url.startsWith('https://i.redd.it/')){
+          return item.url;
+        }
+      }
+      if(item.imageUrl) {
+        return item.imageUrl;
+      }
+    }
+    if(item.display.displayType==='imgur') {
+      return item.url.replace("/a/","/");
+    }
+    if(item.display.displayType==='youtube') {
+      return item.url.replace("https://youtu.be/","https://youtbe.com/embed/");
+    }
+    return undefined;
+  }
+  private getDisplayType = (item:any) => {
+    if(item.url) {
+      if(item.url.startsWith('https://youtu.be/kcIv6JqHRuk')) {
+        console.log("[Youtube]")
+        return 'youtube';
+      }
+      if(item.url.startsWith('https://imgur.com')) {
+        console.log("[Imgur]")
+        return 'imgur'
+      };
+    }
+    return 'image';
   }
 }
