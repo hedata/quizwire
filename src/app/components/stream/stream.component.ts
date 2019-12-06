874c3f8f267e3fe60fb71d4a6d238af180d65d42
hedata <erichheil@gmail.com>
2019-12-06 16:37:13 +0100

    some style tweaks

--- src/app/components/stream/stream.component.ts
+++ src/app/components/stream/stream.component.ts
@@ -16,11 +16,11 @@ const log = new Logger('Entity');
 })
 export class StreamComponent implements OnInit {
   version: string = environment.version;
-  public loading:boolean=true;
-  public streamItems: any =[] ;
-  public fade:boolean = true;
+  public loading: boolean = true;
+  public streamItems: any = [];
+  public fade: boolean = true;
   public language: String = 'en';
-  private lastDate : any = new Date();
+  private lastDate: any = new Date();
 
   @Input() activeComponentConfig: any;
   isActive: boolean = false;
@@ -44,31 +44,32 @@ export class StreamComponent implements OnInit {
     private dataService: DataService
   ) {}
 
+  intervall = 3400;
   ngOnInit() {
     console.log('[Stream] Init');
     //2300
-    const intervall = 3400;
-      setInterval(this.loadData, intervall);
+
+    setInterval(this.loadData, this.intervall);
   }
 
   private loadData = async () => {
-    const currentDate:any = new Date();
-    if(this.isActive) {
+    const currentDate: any = new Date();
+    if (this.isActive) {
       //console.log("[Stream] loding data");
       const countoldItems = this.streamItems.length;
-      const response = (await this.dataService.getLatestRawStream({
-        lastUpdate : this.streamItems.length>0?this.streamItems[0].created_at:null,
-        limit : 1
-      }));
-      if(response.length>0) {
+      const response = await this.dataService.getLatestRawStream({
+        lastUpdate: new Date(currentDate.getTime() - this.intervall),
+        limit: 1
+      });
+      if (response.length > 0) {
         const item = response[0];
         //add displayType and displayUrl
         this.addDisplayTypes(item);
         console.log(item.url);
         console.log(item.display);
-        //console.log(item);
+        console.log(item);
         this.streamItems.unshift(item);
-        if(this.streamItems.length>500) {
+        if (this.streamItems.length > 500) {
           this.streamItems.pop();
         }
         const diff = (currentDate - this.lastDate) / 1000;
@@ -76,56 +77,54 @@ export class StreamComponent implements OnInit {
         //console.log("Start: ",this.lastDate, " End: ",currentDate);
 
         this.lastDate = currentDate;
-        this.fade=false;
-        setTimeout(()=> {
+        this.fade = false;
+        setTimeout(() => {
           //console.log("[Fade]")
           this.fade = true;
-        },1000)
-      } else{
-        console.log("[Got no items]")
-
+        }, 1000);
+      } else {
+        console.log('[Got no items]');
       }
     }
-
-  }
-  addDisplayTypes = (item:any) => {
-    item.display =  {
-      displayType : null,
-      displayUrl : null
+  };
+  addDisplayTypes = (item: any) => {
+    item.display = {
+      displayType: null,
+      displayUrl: null
     };
-    if(item.url) {
-      if(item.url.startsWith('https://www.youtube.com/watch?v=')) {
+    if (item.url) {
+      if (item.url.startsWith('https://www.youtube.com/watch?v=')) {
         item.display.displayType = 'youtube';
-        item.display.displayUrl= item.url.replace("https://www.youtube.com/watch?v=","https://youtube.com/embed/");
+        item.display.displayUrl = item.url.replace('https://www.youtube.com/watch?v=', 'https://youtube.com/embed/');
       }
-      if(item.url.startsWith('https://youtu.be')) {
+      if (item.url.startsWith('https://youtu.be')) {
         //console.log("[Youtube]")
         item.display.displayType = 'youtube';
-        item.display.displayUrl= item.url.replace("https://youtu.be/","https://youtube.com/embed/");
+        item.display.displayUrl = item.url.replace('https://youtu.be/', 'https://youtube.com/embed/');
       }
-      if(item.url.startsWith('https://imgur.com')) {
-        item.display.displayType =  'imgur';
-        item.display.displayUrl = item.url.replace("/a/","/").replace("https://","https://i.")+".jpg";
-      };
-      if(item.url.startsWith('https://i.imgur.com')) {
-        item.display.displayType =  'imgur';
+      if (item.url.startsWith('https://imgur.com')) {
+        item.display.displayType = 'imgur';
+        item.display.displayUrl = item.url.replace('/a/', '/').replace('https://', 'https://i.') + '.jpg';
+      }
+      if (item.url.startsWith('https://i.imgur.com')) {
+        item.display.displayType = 'imgur';
         item.display.displayUrl = item.url;
       }
-      if(item.url.startsWith('https://gfycat.com')) {
-        item.display.displayType =  'gfycat';
-        item.display.displayUrl = item.url.replace('https://gfycat.com/','https://gfycat.com/ifr/');
+      if (item.url.startsWith('https://gfycat.com')) {
+        item.display.displayType = 'gfycat';
+        item.display.displayUrl = item.url.replace('https://gfycat.com/', 'https://gfycat.com/ifr/');
       }
-      if(item.url.startsWith('https://i.redd.it/')){
+      if (item.url.startsWith('https://i.redd.it/')) {
         item.display.displayUrl = item.url;
-        item.display.displayType =  'image';
+        item.display.displayType = 'image';
       }
     }
-    if(!item.display.displayType) {
-      if(item.imageUrl) {
-        item.display.displayType =  'image';
-        item.display.displayUrl =  item.imageUrl;
+    if (!item.display.displayType) {
+      if (item.imageUrl) {
+        item.display.displayType = 'image';
+        item.display.displayUrl = item.imageUrl;
       }
     }
     return item;
-  }
+  };
 }
